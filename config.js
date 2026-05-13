d3873e2068a9a21cac350aec04424bc15c9c1908
sasmithasasmitha040-afk <sasmithasasmitha040@gmail.com>
2026-05-13 12:27:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'qygknJqI#1ddfrN6T4oqCQsLwthFd9uXa9uA7b0ld_X92D3IvYj4' : process.env.SESSION_ID,
 };
